847c6c7eb7175bd66f4e84016ebe8b1809d6cc5f
Wenxuan Zhang <wenxuangm@gmail.com>
2021-12-01 20:28:40 +0800

    refactor

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -4,19 +4,14 @@ fn parse_input(input: &str) -> Result<Vec<u32>> {
     input.split_ascii_whitespace().map(|s| Ok(s.parse()?)).collect()
 }
 
-fn increased_count(seq: &[u32]) -> usize {
-    seq.windows(2).filter(|w| w[1] > w[0]).count()
-}
-
 fn part1(input: &str) -> Result<usize> {
     let seq = parse_input(input)?;
-    Ok(increased_count(&seq))
+    Ok(seq.windows(2).filter(|w| w[1] > w[0]).count())
 }
 
 fn part2(input: &str) -> Result<usize> {
     let seq = parse_input(input)?;
-    let wins: Vec<u32> = seq.windows(3).map(|w| w.iter().sum()).collect();
-    Ok(increased_count(&wins))
+    Ok(seq.windows(4).filter(|w| w[3] > w[0]).count())
 }
 
-solution!(part1 => 0, part2 => 0);
+solution!(part1 => 1390, part2 => 1457);
