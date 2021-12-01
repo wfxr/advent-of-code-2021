2d62636af167dcd9d8df2f4fd566b6f4f363cb5a
Wenxuan Zhang <wenxuangm@gmail.com>
2021-12-01 19:14:05 +0800

    ac: 1.1

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -1,7 +1,17 @@
 use crate::{solution, Result};
 
+fn parse_input(input: &str) -> Result<Vec<u32>> {
+    input.split_ascii_whitespace().map(|s| Ok(s.parse()?)).collect()
+}
+
 fn part1(input: &str) -> Result<usize> {
-    unimplemented!()
+    let seq = parse_input(input)?;
+    Ok(seq
+        .iter()
+        .skip(1)
+        .zip(seq.iter())
+        .filter(|&(curr, prev)| curr > prev)
+        .count())
 }
 
 fn part2(input: &str) -> Result<usize> {
