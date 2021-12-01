37a2f7d7fae3f6924285ef12455b30d2c846f725
Wenxuan Zhang <wenxuangm@gmail.com>
2021-12-01 19:24:53 +0800

    ac: 1.2

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -4,18 +4,19 @@ fn parse_input(input: &str) -> Result<Vec<u32>> {
     input.split_ascii_whitespace().map(|s| Ok(s.parse()?)).collect()
 }
 
+fn increased_count(seq: &[u32]) -> usize {
+    seq.windows(2).filter(|w| w[1] > w[0]).count()
+}
+
 fn part1(input: &str) -> Result<usize> {
     let seq = parse_input(input)?;
-    Ok(seq
-        .iter()
-        .skip(1)
-        .zip(seq.iter())
-        .filter(|&(curr, prev)| curr > prev)
-        .count())
+    Ok(increased_count(&seq))
 }
 
 fn part2(input: &str) -> Result<usize> {
-    unimplemented!()
+    let seq = parse_input(input)?;
+    let wins: Vec<u32> = seq.windows(3).map(|w| w.iter().sum()).collect();
+    Ok(increased_count(&wins))
 }
 
 solution!(part1 => 0, part2 => 0);
