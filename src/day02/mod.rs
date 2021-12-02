d21a93515595da520dbec259baf2de4cabea61bb
Wenxuan Zhang <wenxuangm@gmail.com>
2021-12-02 16:22:16 +0800

    ac: 2.2

--- src/day02/mod.rs
+++ src/day02/mod.rs
@@ -1,26 +1,41 @@
+use self::Ins::*;
 use crate::{solution, Result};
 
-fn parse_input(input: &str) -> Result<Vec<(i32, i32)>> {
+enum Ins {
+    Forward(i32),
+    Down(i32),
+}
+
+fn parse_input(input: &str) -> Result<Vec<Ins>> {
     input
         .lines()
         .map(|line| match line.split_ascii_whitespace().collect::<Vec<_>>()[..] {
-            ["forward", x] => Ok((0, x.parse()?)),
-            ["up", x] => Ok((-x.parse::<i32>()?, 0)),
-            ["down", x] => Ok((x.parse()?, 0)),
+            ["forward", x] => Ok(Forward(x.parse()?)),
+            ["up", x] => Ok(Down(-x.parse()?)),
+            ["down", x] => Ok(Down(x.parse()?)),
             _ => Err(format!("invalid input: {}", line).into()),
         })
         .collect()
 }
 
-fn part1(input: &str) -> Result<usize> {
+fn part1(input: &str) -> Result<i32> {
     let (x, y) = parse_input(input)?
         .into_iter()
-        .fold((0, 0), |(x, y), (dx, dy)| (x + dx, y + dy));
-    Ok(x as usize * y as usize)
+        .fold((0, 0), |(horizontal, depth), ins| match ins {
+            Forward(n) => (horizontal + n, depth),
+            Down(n) => (horizontal, depth + n),
+        });
+    Ok(x * y)
 }
 
-fn part2(_input: &str) -> Result<usize> {
-    unimplemented!()
+fn part2(input: &str) -> Result<i32> {
+    let (horizontal, depth, _) = parse_input(input)?
+        .into_iter()
+        .fold((0, 0, 0), |(horizontal, depth, aim), ins| match ins {
+            Forward(n) => (horizontal + n, depth + n * aim, aim),
+            Down(n) => (horizontal, depth, aim + n),
+        });
+    Ok(horizontal * depth)
 }
 
-solution!(part1 => 1924923, part2 => 0);
+solution!(part1 => 1924923, part2 => 1982495697);
