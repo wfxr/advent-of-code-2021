39c3c9e1d7cf5c6e67020543c5a7f709c4d506ed
Wenxuan Zhang <wenxuangm@gmail.com>
2021-12-04 10:44:34 +0800

    refactor

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -16,21 +16,22 @@ fn part1(input: &str) -> Result<u32> {
 }
 
 fn part2(input: &str) -> Result<u32> {
-    let life_support_rating = |most_common: bool| -> Result<u32> {
+    let rating = |most_common: bool| -> Result<u32> {
         let mut seq: Vec<_> = input.lines().collect();
         let mut col = 0;
         while seq.len() > 1 {
             let ones = seq.iter().filter(|l| l.as_bytes()[col] == b'1').count();
-            let target = match (most_common, ones * 2 >= seq.len()) {
+            let bit = match (most_common, ones * 2 >= seq.len()) {
                 (true, true) | (false, false) => b'1',
                 _ => b'0',
             };
-            seq = seq.into_iter().filter(|l| l.as_bytes()[col] == target).collect();
+            seq = seq.into_iter().filter(|l| l.as_bytes()[col] == bit).collect();
             col += 1;
         }
         Ok(u32::from_str_radix(seq.first().ok_or("empty input")?, 2)?)
     };
-    Ok(life_support_rating(true)? * life_support_rating(false)?)
+    let (oxygen, co2) = (rating(true)?, rating(false)?);
+    Ok(oxygen * co2)
 }
 
 solution!(part1 => 841526, part2 => 4790390);
