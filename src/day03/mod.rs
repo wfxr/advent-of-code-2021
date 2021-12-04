5bcd3edd170a22cca81cc5a99bc56941df25bb58
Wenxuan Zhang <wenxuangm@gmail.com>
2021-12-04 09:45:01 +0800

    refactor

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -1,57 +1,36 @@
 use crate::{solution, Result};
 
-fn part1(input: &str) -> Result<usize> {
+fn part1(input: &str) -> Result<u32> {
     let cols = input.lines().next().ok_or("empty input")?.len();
-    let mut v = vec![0; cols];
-    let mut rows = 0;
-    for line in input.lines() {
-        rows += 1;
-        for (col, c) in line.bytes().enumerate() {
-            v[col] += (c - b'0') as usize
-        }
-    }
-    let mut gamma = 0;
-    let mut epsilon = 0;
-    for &n in &v {
-        gamma <<= 1;
-        epsilon <<= 1;
-        if n > rows / 2 {
-            gamma |= 1;
-        } else {
-            epsilon |= 1;
-        }
-    }
+    let rows = input.lines().count();
+    let (gamma, epsilon) = (0..cols)
+        .map(|i| input.lines().filter(|line| line.as_bytes()[i] == b'1').count())
+        .fold((0, 0), |(gamma, epsilon), ones| {
+            if ones * 2 > rows {
+                ((gamma << 1) | 1, epsilon << 1)
+            } else {
+                (gamma << 1, (epsilon << 1) | 1)
+            }
+        });
     Ok(gamma * epsilon)
 }
 
 fn part2(input: &str) -> Result<u32> {
-    let seq: Vec<_> = input.lines().collect();
-    let oxygen = life_support_rating(&seq, |len, ones| if ones * 2 >= len { b'1' } else { b'0' })?;
-    let co2 = life_support_rating(&seq, |len, ones| if ones * 2 >= len { b'0' } else { b'1' })?;
-    Ok(oxygen * co2)
-}
-
-fn life_support_rating<F>(seq: &[&str], target_fn: F) -> Result<u32>
-where
-    F: Fn(usize, usize) -> u8,
-{
-    let mut seq: Vec<_> = seq.iter().map(|line| line.as_bytes()).collect();
-    let mut col = 0;
-    while seq.len() > 1 {
-        let mut ones = 0;
-        for c in seq.iter().map(move |line| line[col]) {
-            if c == b'1' {
-                ones += 1;
-            }
+    let life_support_rating = |most_common: bool| -> Result<u32> {
+        let mut seq: Vec<_> = input.lines().collect();
+        let mut col = 0;
+        while seq.len() > 1 {
+            let ones = seq.iter().filter(|l| l.as_bytes()[col] == b'1').count();
+            let target = match (most_common, ones * 2 >= seq.len()) {
+                (true, true) | (false, false) => b'1',
+                _ => b'0',
+            };
+            seq = seq.into_iter().filter(|l| l.as_bytes()[col] == target).collect();
+            col += 1;
         }
-        let target = target_fn(seq.len(), ones);
-        seq = seq.into_iter().filter(|line| line[col] == target).collect();
-        col += 1;
-    }
-    Ok(match seq.first() {
-        Some(line) => u32::from_str_radix(std::str::from_utf8(line)?, 2)?,
-        None => 0,
-    })
+        Ok(u32::from_str_radix(seq.first().ok_or("empty input")?, 2)?)
+    };
+    Ok(life_support_rating(true)? * life_support_rating(false)?)
 }
 
 solution!(part1 => 841526, part2 => 4790390);
