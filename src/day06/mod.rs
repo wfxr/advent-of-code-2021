2c36c0402a8cb6fbdfdcece1b0b33e37637cc54b
Wenxuan Zhang <wenxuangm@gmail.com>
2021-12-06 18:40:56 +0800

    perf

--- src/day06/mod.rs
+++ src/day06/mod.rs
@@ -1,33 +1,22 @@
 use crate::{solution, Result};
 
-fn parse_input(input: &str) -> Result<[usize; 9]> {
-    input
-        .split(',')
-        .map(|x| x.trim().parse::<usize>())
-        .try_fold([0; 9], |mut school, fish| {
-            school[fish?] += 1;
-            Ok(school)
-        })
-}
-
-fn count_fish(school: [usize; 9], days: usize) -> usize {
-    (0..9)
-        .cycle()
-        .take(days)
-        .fold(school, |mut s, i| {
-            s[(i + 7) % 9] += s[i];
-            s
-        })
-        .iter()
-        .sum()
+fn count_fish(input: &str, days: usize) -> Result<usize> {
+    let mut school = [0; 9];
+    for x in input.split(',').map(|x| x.trim().parse::<usize>()) {
+        school[x?] += 1
+    }
+    for i in (0..9).cycle().take(days) {
+        school[(i + 7) % 9] += school[i] // let the new fish inplace and accumulate the old fish
+    }
+    Ok(school.iter().sum())
 }
 
 fn part1(input: &str) -> Result<usize> {
-    Ok(count_fish(parse_input(input)?, 80))
+    count_fish(input, 80)
 }
 
 fn part2(input: &str) -> Result<usize> {
-    Ok(count_fish(parse_input(input)?, 256))
+    count_fish(input, 256)
 }
 
 solution!(part1 => 379414, part2 => 1705008653296);
