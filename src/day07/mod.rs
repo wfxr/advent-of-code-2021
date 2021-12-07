d66415c6f3b0107d55c214f82fc649e076fbf034
Wenxuan Zhang <wenxuangm@gmail.com>
2021-12-07 18:56:47 +0800

    perf

--- src/day07/mod.rs
+++ src/day07/mod.rs
@@ -1,21 +1,28 @@
 use crate::{solution, Result};
 
-#[inline]
-fn fuel_cost(input: &str, f: fn(u32) -> u32) -> Result<u32> {
-    let input: Vec<u32> = input.split(',').map(|x| Ok(x.trim().parse()?)).collect::<Result<_>>()?;
-    let max = *input.iter().max().unwrap_or(&0);
-    Ok((0..=max)
-        .map(|i| input.iter().fold(0, |acc, x| acc + f(x.abs_diff(i))))
-        .min()
-        .unwrap_or(0))
+fn fuel_cost(input: &str, pos_fn: fn(&mut [u32]) -> u32, fuel_fn: fn(u32) -> u32) -> Result<u32> {
+    let mut input: Vec<u32> = input.split(',').map(|x| Ok(x.trim().parse()?)).collect::<Result<_>>()?;
+    let pos = pos_fn(&mut input);
+    Ok(input.iter().fold(0, |acc, x| acc + fuel_fn(x.abs_diff(pos))))
 }
 
 fn part1(input: &str) -> Result<u32> {
-    fuel_cost(input, |dis| dis)
+    fuel_cost(
+        input,
+        |seq| {
+            let (_, &mut median, _) = seq.select_nth_unstable(seq.len() / 2);
+            median
+        },
+        |d| d,
+    )
 }
 
 fn part2(input: &str) -> Result<u32> {
-    fuel_cost(input, |dis| dis * (dis + 1) / 2)
+    fuel_cost(
+        input,
+        |seq| seq.iter().sum::<u32>() / seq.len() as u32, // don't ask me why
+        |d| d * (d + 1) / 2,
+    )
 }
 
 solution!(part1 => 340987, part2 => 96987874);
