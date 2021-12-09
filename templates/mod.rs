db1abde57dda80c73bc4e8a87b52b3885f74a06c
Wenxuan Zhang <wenxuangm@gmail.com>
2021-12-09 13:06:48 +0800

    update template

--- templates/mod.rs
+++ templates/mod.rs
@@ -1,11 +1,11 @@
 use crate::{solution, Result};
 
-fn part1(input: &str) -> Result<usize> {
-    unimplemented!()
+fn part1(_input: &str) -> Result<usize> {
+    todo!()
 }
 
-fn part2(input: &str) -> Result<usize> {
-    unimplemented!()
+fn part2(_input: &str) -> Result<usize> {
+    todo!()
 }
 
-solution!(part1 => 0, part2 => 0);
+solution!(part1 => todo!(), part2 => todo!());
