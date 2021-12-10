1bb17115c769866fdfd7fafc320d2208df0deef1
Wenxuan Zhang <wenxuangm@gmail.com>
2021-12-10 14:58:59 +0800

    ac: 10.2

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -29,8 +29,49 @@ fn part1(input: &str) -> Result<usize> {
         .sum())
 }
 
-fn part2(_input: &str) -> Result<usize> {
-    todo!()
+fn part2(input: &str) -> Result<usize> {
+    let mut scores: Vec<_> = input
+        .lines()
+        .map(|line| {
+            let mut st = Vec::new();
+            macro_rules! score {
+                ($lhs:pat, $rhs:expr, $score:expr) => {
+                    match st.pop() {
+                        Some($lhs) => None,
+                        _ => Some($score),
+                    }
+                };
+            }
+            match line.chars().find_map(|c| match c {
+                ')' => score!('(', c, 3),
+                ']' => score!('[', c, 57),
+                '}' => score!('{', c, 1197),
+                '>' => score!('<', c, 25137),
+                _ => {
+                    st.push(c);
+                    None
+                }
+            }) {
+                Some(_) => 0,
+                None => match st.is_empty() {
+                    true => 0,
+                    false => st.into_iter().rev().fold(0, |acc, c| {
+                        acc * 5
+                            + match c {
+                                '(' => 1,
+                                '[' => 2,
+                                '{' => 3,
+                                '<' => 4,
+                                _ => 0,
+                            }
+                    }),
+                },
+            }
+        })
+        .filter(|&x| x > 0)
+        .collect();
+    let middle = scores.len() / 2;
+    Ok(*scores.select_nth_unstable(middle).1)
 }
 
 solution!(part1 => todo!(), part2 => todo!());
