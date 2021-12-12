f1760457a39bc51e376be531b398c4e18dea6a6d
Wenxuan Zhang <wenxuangm@gmail.com>
2021-12-12 19:34:47 +0800

    refactor

--- src/day12/mod.rs
+++ src/day12/mod.rs
@@ -1,87 +1,78 @@
-use std::collections::HashMap;
-
 use crate::{solution, Result};
+use std::{cell::Cell, collections::HashMap, rc::Rc};
 
-type Edges<'a> = HashMap<&'a str, Vec<&'a str>>;
-type Counter<'a> = HashMap<&'a str, isize>;
+type Edges<'a> = HashMap<&'a str, (Vec<&'a str>, Rc<Cell<isize>>)>;
 
-fn parse_input(input: &str) -> (Edges, Counter) {
-    let edges = input
+fn parse_input(input: &str) -> Edges {
+    input
         .lines()
         .filter_map(|line| line.split_once('-'))
         .flat_map(|(l, r)| [(l, r), (r, l)])
         .fold(HashMap::new(), |mut acc, (from, to)| {
-            acc.entry(from).or_insert(vec![]).push(to);
+            acc.entry(from)
+                .or_insert_with_key(|from| {
+                    let nodes = vec![];
+                    let count = match from {
+                        from if from.chars().any(|c| c.is_ascii_uppercase()) => -1,
+                        _ => 1,
+                    };
+                    (nodes, Rc::new(Cell::new(count)))
+                })
+                .0
+                .push(to);
             acc
-        });
-    let counter: HashMap<_, _> = edges
-        .keys()
-        .map(|&k| {
-            (k, match k {
-                k if k.chars().any(|c| c.is_ascii_uppercase()) => -1,
-                _ => 1,
-            })
         })
-        .collect();
-    (edges, counter)
 }
 
 fn part1(input: &str) -> Result<usize> {
-    let (edges, mut counter) = parse_input(input);
-    Ok(dfs(&edges, &mut counter, "start"))
-}
-
-fn dfs(edges: &Edges, counter: &mut Counter, curr: &str) -> usize {
-    match counter[curr] {
-        0 => 0,
-        _ => {
-            if let Some(n) = counter.get_mut(curr) {
-                *n -= 1
+    fn dfs(edges: &Edges, curr: &str) -> usize {
+        let (nodes, count) = &edges[curr];
+        let count = Rc::clone(count);
+        match count.get() {
+            0 => 0,
+            n => {
+                count.set(n - 1);
+                let paths = nodes.iter().map(|x| dfs(edges, *x)).sum::<usize>();
+                count.set(n);
+                paths
+                    + match curr {
+                        "end" => 1,
+                        _ => 0,
+                    }
             }
-            let count = edges[curr].iter().map(|x| dfs(edges, counter, *x)).sum::<usize>();
-            if let Some(n) = counter.get_mut(curr) {
-                *n += 1
-            }
-            count
-                + match curr {
-                    "end" => 1,
-                    _ => 0,
-                }
         }
     }
+    Ok(dfs(&parse_input(input), "start"))
 }
 
 fn part2(input: &str) -> Result<usize> {
-    let (edges, mut counter) = parse_input(input);
-    Ok(dfs2(&edges, &mut counter, "start", 1))
-}
-
-fn dfs2(edges: &Edges, counter: &mut Counter, curr: &str, extra: usize) -> usize {
-    match (curr, counter[curr], extra) {
-        ("start" | "end", 0, _) => 0,
-        (_, 0, 0) => 0,
-        (_, n, extra) => {
-            if n != 0 {
-                if let Some(n) = counter.get_mut(curr) {
-                    *n -= 1
+    fn dfs(edges: &Edges, curr: &str, extra: usize) -> usize {
+        let (nodes, count) = &edges[curr];
+        let count = Rc::clone(count);
+        match (curr, count.get(), extra) {
+            ("start" | "end", 0, _) => 0,
+            (_, 0, 0) => 0,
+            (_, n, mut extra) => {
+                match n {
+                    0 => extra -= 1,
+                    _ => count.set(n - 1),
                 }
+                let paths = nodes.iter().map(|x| dfs(edges, *x, extra)).sum::<usize>();
+                if n == 0 {
+                    count.set(n)
+                }
+                if n != 0 {
+                    count.set(n)
+                }
+                paths
+                    + match curr {
+                        "end" => 1,
+                        _ => 0,
+                    }
             }
-            let count = edges[curr]
-                .iter()
-                .map(|x| dfs2(edges, counter, *x, if n == 0 { extra - 1 } else { extra }))
-                .sum::<usize>();
-            if n != 0 {
-                if let Some(n) = counter.get_mut(curr) {
-                    *n += 1
-                }
-            }
-            count
-                + match curr {
-                    "end" => 1,
-                    _ => 0,
-                }
         }
     }
+    Ok(dfs(&parse_input(input), "start", 1))
 }
 
 solution!(part1 => 3421, part2 => 84870);
