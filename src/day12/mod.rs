7d4f090b38bec11a0ce8627147ae41669b39a7a5
Wenxuan Zhang <wenxuangm@gmail.com>
2021-12-12 19:41:51 +0800

    refactor

--- src/day12/mod.rs
+++ src/day12/mod.rs
@@ -11,11 +11,8 @@ fn parse_input(input: &str) -> Edges {
         .fold(HashMap::new(), |mut acc, (from, to)| {
             acc.entry(from)
                 .or_insert_with_key(|from| {
-                    let nodes = vec![];
-                    let count = match from {
-                        from if from.chars().any(|c| c.is_ascii_uppercase()) => -1,
-                        _ => 1,
-                    };
+                    let nodes = Vec::new();
+                    let count = if from.chars().any(|c| c.is_uppercase()) { -1 } else { 1 };
                     (nodes, Rc::new(Cell::new(count)))
                 })
                 .0
@@ -34,11 +31,7 @@ fn part1(input: &str) -> Result<usize> {
                 count.set(n - 1);
                 let paths = nodes.iter().map(|x| dfs(edges, *x)).sum::<usize>();
                 count.set(n);
-                paths
-                    + match curr {
-                        "end" => 1,
-                        _ => 0,
-                    }
+                paths + if curr == "end" { 1 } else { 0 }
             }
         }
     }
@@ -50,25 +43,17 @@ fn part2(input: &str) -> Result<usize> {
         let (nodes, count) = &edges[curr];
         let count = Rc::clone(count);
         match (curr, count.get(), extra) {
-            ("start" | "end", 0, _) => 0,
-            (_, 0, 0) => 0,
+            ("start" | "end", 0, _) | (_, 0, 0) => 0,
             (_, n, mut extra) => {
                 match n {
                     0 => extra -= 1,
                     _ => count.set(n - 1),
                 }
                 let paths = nodes.iter().map(|x| dfs(edges, *x, extra)).sum::<usize>();
-                if n == 0 {
-                    count.set(n)
-                }
                 if n != 0 {
                     count.set(n)
                 }
-                paths
-                    + match curr {
-                        "end" => 1,
-                        _ => 0,
-                    }
+                paths + if curr == "end" { 1 } else { 0 }
             }
         }
     }
