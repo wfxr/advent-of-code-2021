7163d501f599c9bd94b90498eeef6707e7c8c8a6
Wenxuan Zhang <wenxuangm@gmail.com>
2021-12-13 16:20:50 +0800

    perf

--- src/day12/mod.rs
+++ src/day12/mod.rs
@@ -5,7 +5,7 @@ const START: usize = 0;
 const END: usize = 1;
 
 fn parse_input(input: &str) -> Vec<Vec<usize>> {
-    let m = input.lines().flat_map(|line| line.split('-')).fold(
+    let id = input.lines().flat_map(|line| line.split('-')).fold(
         HashMap::from([("start", START), ("end", END)]),
         |mut acc, cave| {
             let id = acc.len();
@@ -13,52 +13,54 @@ fn parse_input(input: &str) -> Vec<Vec<usize>> {
             acc
         },
     );
-    let mut rs: Vec<Vec<usize>> = input
+
+    let mut caves = vec![vec![]; id.len()];
+    input
         .lines()
-        .filter_map(|line| line.split_once('-').map(|(l, r)| (m[l], m[r])))
+        .filter_map(|line| line.split_once('-').map(|(l, r)| (id[l], id[r])))
         .flat_map(|(l, r)| [(l, r), (r, l)])
-        .fold(Vec::new(), |mut acc, (l, r)| {
-            (acc.len() <= l).then(|| acc.resize(l + 1, Vec::new()));
-            acc[l].push(r);
-            acc
-        });
-    m.iter()
+        .filter(|(l, r)| l != &END && r != &START)
+        .for_each(|(l, r)| caves[l].push(r));
+
+    // flatten big caves
+    id.iter()
         .filter(|&(name, _)| name.chars().any(|c| c.is_uppercase()))
         .for_each(|(_, &x)| {
-            let smalls = rs[x].clone();
-            for nodes in rs.iter_mut() {
-                if nodes.contains(&x) {
-                    nodes.drain_filter(|s| s == &x);
-                    nodes.extend(smalls.iter());
-                }
-            }
+            let smalls = caves[x].clone();
+            caves.iter_mut().filter(|nodes| nodes.contains(&x)).for_each(|nodes| {
+                nodes.drain_filter(|s| s == &x);
+                nodes.extend(smalls.iter());
+            });
         });
-    rs
+    caves
 }
 
-fn dfs(edges: &[Vec<usize>], visited: &mut Vec<bool>, curr: usize, extra: usize) -> usize {
-    edges[curr].iter().fold(0, |acc, &next| match next {
-        next if next == START => acc,
-        next if next == END => acc + 1,
-        next if visited[next] && extra == 0 => acc,
-        next if !visited[next] => {
-            visited[next] = true;
-            let paths = dfs(edges, visited, next, extra);
-            visited[next] = false;
-            acc + paths
-        }
-        next => acc + dfs(edges, visited, next, extra - 1),
+fn dfs(caves: &[Vec<usize>], visited: &mut Vec<bool>, curr: usize, extra: usize) -> usize {
+    caves[curr].iter().fold(0, |acc, &next| match next {
+        END => acc + 1,
+        _ => match visited[next] {
+            true => match extra {
+                0 => acc,
+                _ => acc + dfs(caves, visited, next, extra - 1),
+            },
+            _ => {
+                visited[next] = true;
+                let paths = dfs(caves, visited, next, extra);
+                visited[next] = false;
+                acc + paths
+            }
+        },
     })
 }
 
 fn part1(input: &str) -> Result<usize> {
-    let edges = parse_input(input);
-    Ok(dfs(&edges, &mut vec![false; edges.len()], START, 0))
+    let caves = parse_input(input);
+    Ok(dfs(&caves, &mut vec![false; caves.len()], START, 0))
 }
 
 fn part2(input: &str) -> Result<usize> {
-    let edges = parse_input(input);
-    Ok(dfs(&edges, &mut vec![false; edges.len()], START, 1))
+    let caves = parse_input(input);
+    Ok(dfs(&caves, &mut vec![false; caves.len()], START, 1))
 }
 
 solution!(part1 => 3421, part2 => 84870);
