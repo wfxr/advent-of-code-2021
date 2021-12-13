229632fafbeb04a2c67a4062529e978aaf7f2f82
Wenxuan Zhang <wenxuangm@gmail.com>
2021-12-13 13:08:05 +0800

    refactor

--- src/day11/mod.rs
+++ src/day11/mod.rs
@@ -1,8 +1,8 @@
 use crate::{solution, Result};
 
 const N: usize = 10;
-const THRESHOLD: i8 = 10;
-type Octopuses = [i8; N * N];
+const THRESHOLD: u8 = 10;
+type Octopuses = [u8; N * N];
 
 fn part1(input: &str) -> Result<usize> {
     let octopuses = &mut parse_input(input)?;
@@ -20,31 +20,27 @@ fn parse_input(input: &str) -> Result<Octopuses> {
     input
         .bytes()
         .filter(|&c| c.is_ascii_digit())
-        .map(|c| (c - b'0') as i8)
+        .map(|c| c - b'0')
         .collect::<Vec<_>>()
         .try_into()
         .map_err(|_| format!("not a {N} x {N} array").into())
 }
 
 fn next_step(octopuses: &mut Octopuses) -> usize {
-    octopuses.iter_mut().for_each(|x| match *x {
-        -1 => *x = 1,
-        _ => *x += 1,
-    });
+    octopuses.iter_mut().for_each(|x| *x += 1);
     (0..N * N)
         .filter_map(|pos| (octopuses[pos] >= THRESHOLD).then(|| flash(octopuses, pos)))
         .sum()
 }
 
 fn flash(octopuses: &mut Octopuses, pos: usize) -> usize {
-    octopuses[pos] = -1;
+    octopuses[pos] = 0;
     1 + [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
         .into_iter()
         .map(move |(di, dj)| (((pos / N) as isize + di) as usize, ((pos % N) as isize + dj) as usize))
-        .filter(|&(ii, jj)| ii < N && jj < N)
-        .map(|(ii, jj)| ii * N + jj)
+        .filter_map(|(ii, jj)| (ii < N && jj < N).then(|| ii * N + jj))
         .filter_map(|pos| {
-            (octopuses[pos] != -1)
+            (octopuses[pos] > 0)
                 .then(|| octopuses[pos] += 1)
                 .and((octopuses[pos] >= THRESHOLD).then(|| flash(octopuses, pos)))
         })
