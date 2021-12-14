8dd3989b87ec403312a6ccdd294a7a82145c97e7
Wenxuan Zhang <wenxuangm@gmail.com>
2021-12-14 18:52:35 +0800

    refactor

--- src/day14/mod.rs
+++ src/day14/mod.rs
@@ -1,10 +1,14 @@
 use crate::{solution, Result};
 
 fn solve(input: &str, loops: usize) -> Result<usize> {
+    const N: usize = 26;
     let id = |b| (b - b'A') as usize;
-    let (template, rules) = input.split_once("\n\n").ok_or("missing rules")?;
-    let template = template.bytes().map(id).collect::<Vec<_>>();
-    let rules = rules
+    let left = |i| i / N;
+    let right = |i| i % N;
+
+    let (init, rules) = input.split_once("\n\n").ok_or("missing rules")?;
+    let init = init.bytes().map(id).collect::<Vec<_>>();
+    let trans = rules
         .lines()
         .map(|l| l.as_bytes())
         .filter_map(|l| Some((l.get(0)?, l.get(1)?, l.get(6)?)))
@@ -13,28 +17,26 @@ fn solve(input: &str, loops: usize) -> Result<usize> {
             acc[k] = v;
             acc
         });
-    let mut pairs = [0; N * N];
-    template
-        .array_windows()
-        .map(|&[a, b]| a * N + b)
-        .for_each(|x| pairs[x] += 1);
+
+    let mut curr = [0; N * N];
+    init.array_windows().map(|&[a, b]| a * N + b).for_each(|x| curr[x] += 1);
 
     for _ in 0..loops {
-        let mut new_pairs = [0; N * N];
-        for (i, &x) in pairs.iter().enumerate() {
-            new_pairs[i / N * N + rules[i]] += x;
-            new_pairs[rules[i] * N + i % N] += x;
+        let mut next = [0; N * N];
+        for (i, &n) in curr.iter().enumerate() {
+            next[left(i) * N + trans[i]] += n;
+            next[trans[i] * N + right(i)] += n;
         }
-        pairs = new_pairs;
+        curr = next;
     }
 
     let mut counter = [0; N];
-    pairs.iter().enumerate().for_each(|(i, &x)| {
-        counter[i / N] += x;
-        counter[i % N] += x;
+    curr.iter().enumerate().for_each(|(i, &x)| {
+        counter[left(i)] += x;
+        counter[right(i)] += x;
     });
-    counter[*template.first().unwrap()] += 1;
-    counter[*template.last().unwrap()] += 1;
+    counter[*init.first().unwrap()] += 1;
+    counter[*init.last().unwrap()] += 1;
     let (min, max) = counter
         .iter()
         .filter(|&&x| x > 0)
@@ -45,8 +47,6 @@ fn solve(input: &str, loops: usize) -> Result<usize> {
 fn part1(input: &str) -> Result<usize> {
     solve(input, 10)
 }
-
-const N: usize = 26;
 
 fn part2(input: &str) -> Result<usize> {
     solve(input, 40)
