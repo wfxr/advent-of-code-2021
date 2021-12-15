5bc9af7a057cc554c5a45740dd17795536f8a709
Wenxuan Zhang <wenxuangm@gmail.com>
2021-12-15 20:18:06 +0800

    refactor

--- src/day15/mod.rs
+++ src/day15/mod.rs
@@ -1,50 +1,46 @@
+use crate::{solution, Result};
 use std::{cmp::Reverse, collections::BinaryHeap};
 
-use crate::{solution, Result};
+type Map<const N: usize> = [[u8; N]; N];
 
-fn parse_input(input: &str) -> (Vec<u8>, (usize, usize)) {
-    let height = input.lines().count();
-    let width = input.lines().next().map(|l| l.len()).unwrap_or(0);
-    let input = input.lines().flat_map(|l| l.bytes().map(|c| c - b'0')).collect();
-    (input, (height, width))
+fn parse_input<const N: usize>(input: &str) -> Result<Map<N>> {
+    input
+        .lines()
+        .map(|l| {
+            l.bytes()
+                .map(|c| c - b'0')
+                .collect::<Vec<_>>()
+                .try_into()
+                .map_err(|_| "invalid row".into())
+        })
+        .collect::<Result<Vec<_>>>()?
+        .try_into()
+        .map_err(|_| "invalid input".into())
 }
 
 fn part1(input: &str) -> Result<u32> {
-    let (input, (height, width)) = parse_input(input);
-    Ok(shortest_path(&input, height, width))
+    Ok(shortest_path(&mut parse_input::<100>(input)?))
 }
 
 fn part2(_input: &str) -> Result<usize> {
     todo!()
 }
 
-fn shortest_path(m: &[u8], h: usize, w: usize) -> u32 {
-    let mut dist: Vec<_> = (0..h * w).map(|_| u32::MAX).collect();
-    let mut heap = BinaryHeap::new();
-
-    dist[0] = 0;
-    heap.push((Reverse(0), 0));
-
-    let neighbors = |pos: usize| {
-        let (i, j) = (pos / h, pos % h);
-        [(i.wrapping_sub(1), j), (i + 1, j), (i, j.wrapping_sub(1)), (i, j + 1)]
-            .into_iter()
-            .filter_map(|(i, j)| (i < h && j < w).then(|| i * w + j))
-    };
-    while let Some((Reverse(risk), pos)) = heap.pop() {
-        if pos == h * w - 1 {
+fn shortest_path<const N: usize>(m: &mut Map<N>) -> u32 {
+    let mut heap = BinaryHeap::from([(Reverse(0), (0, 0))]);
+    while let Some((Reverse(risk), (i, j))) = heap.pop() {
+        if (i, j) == (N - 1, N - 1) {
             return risk;
         }
-        if risk > dist[pos] {
-            continue;
-        }
-        neighbors(pos).for_each(|pos| {
-            let risk = risk + m[pos] as u32;
-            if risk < dist[pos] {
-                heap.push((Reverse(risk), pos));
-                dist[pos] = risk;
-            }
-        })
+        [(i.wrapping_sub(1), j), (i + 1, j), (i, j.wrapping_sub(1)), (i, j + 1)]
+            .into_iter()
+            .filter(|&(i, j)| i < N && j < N)
+            .for_each(|(i, j)| {
+                if m[i][j] > 0 {
+                    heap.push((Reverse(risk + m[i][j] as u32), (i, j)));
+                    m[i][j] = 0;
+                }
+            })
     }
     0
 }
